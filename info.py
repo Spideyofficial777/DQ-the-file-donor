74288eb12f25cf4b7daa9e4fb1b2ff991b3d9522
Spideyofficial777 <gaminghatyar777@gmail.com>
2025-02-07 19:50:27 +0530

    Update info.py

--- info.py
+++ info.py
@@ -43,8 +43,8 @@ NO_RESULTS_MSG = is_enabled((environ.get("NO_RESULTS_MSG", 'False')), False)
 
 # MongoDB information
 SECONDDB_URI = environ.get('SECONDDB_URI', None)
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://spideyofficial777:SPIDEY777@spidey777.pykfj.mongodb.net/SPIDEYDB?retryWrites=true&w=majority&appName=SPIDEY777")
-DATABASE_NAME = environ.get('DATABASE_NAME', "SPIDEY777")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://voromil970:q9aLNL7nsD8EDzwL@cluster0.rlvax.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
