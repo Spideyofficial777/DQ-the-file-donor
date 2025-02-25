686013b84840add5210b0d308e62875632dcef98
Spideyofficial777 <gaminghatyar777@gmail.com>
2025-02-25 09:13:22 +0530

    Update info.py

--- info.py
+++ info.py
@@ -13,7 +13,7 @@ def is_enabled(value, default):
 
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ['API_ID', '28519661'])
+API_ID = int(environ.get('API_ID', '28519661'))
 API_HASH = environ['API_HASH', 'd47c74c8a596fd3048955b322304109d']
 BOT_TOKEN = environ['BOT_TOKEN', '']
 
